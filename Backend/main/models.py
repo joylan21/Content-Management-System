9d3c36afd0cb72de588f1c01f1f8e9615c7e4d4c
Joylan dsouza <joylan.dsouza@datenwissen.com>
2023-04-02 00:40:21 +0530

    Created custom user model with email as the unique identifier instead of username and CustomUserManager with create_user and create_superuser methods

--- Backend/main/models.py
+++ Backend/main/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a new user with the given email and password.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Creates and saves a new superuser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model with email as the unique identifier instead of username.
+    """
+    email = models.EmailField(unique=True, max_length=255)
+    full_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=10)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    state = models.CharField(max_length=255, blank=True, null=True)
+    country = models.CharField(max_length=255, blank=True, null=True)
+    pincode = models.CharField(max_length=6)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name', 'phone', 'pincode']
+
+    def __str__(self):
+        return self.email
