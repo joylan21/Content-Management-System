9321365b68386d21e70de15d4c24df9f2d4e2c33
Joylan dsouza <joylan.dsouza@datenwissen.com>
2023-04-02 13:01:07 +0530

    RegistrationView view function added to hanle user registration api

--- Backend/main/views.py
+++ Backend/main/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserSerializer
 
-# Create your views here.
+class RegistrationView(generics.CreateAPIView):
+    """
+    API view to handle user registration.
+
+    POST request with the following data in the request body:
+    {
+        "email": string,
+        "full_name": string,
+        "phone": int,
+        "address": string,
+        "city": string,
+        "state": string,
+        "country": string,
+        "pincode": int,
+        "password": string
+    }
+
+    Returns a JWT refresh and access token if the user is created successfully.
+    """
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        """
+        This function handles post request to the register api
+        """
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }, status=status.HTTP_201_CREATED)
