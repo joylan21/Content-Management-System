07d97325723b73d7aa4d75d6aaf0eec30a6cb791
Joylan dsouza <joylan.dsouza@datenwissen.com>
2023-04-02 23:28:16 +0530

    url test cases written for content list urls

--- Backend/main/tests/test_urls.py
+++ Backend/main/tests/test_urls.py
@@ -3,7 +3,8 @@ from django.urls import reverse,resolve
 from rest_framework_simplejwt.views import TokenRefreshView
 from main.views import (
     RegistrationView,
-    LoginView
+    LoginView,
+    content_list_view
 )
 
 class TestUrls(SimpleTestCase):
@@ -19,3 +20,9 @@ class TestUrls(SimpleTestCase):
     def test_refresh_token_url_is_resolved(self):
         url = reverse('token_refresh')
         self.assertEquals(resolve(url).func.view_class,TokenRefreshView)
+
+    def test_content_list_url_is_resolved(self):
+        url = reverse('content-list')
+        self.assertEquals(resolve(url).func,content_list_view)
+
+    
