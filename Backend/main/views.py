7319827579ec85c80f8f74491f8367e7671f4caa
Joylan dsouza <joylan.dsouza@datenwissen.com>
2023-04-02 13:18:49 +0530

    LoginView API view added to handle user login

--- Backend/main/views.py
+++ Backend/main/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import UserSerializer
+from .serializers import UserSerializer,LoginSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 class RegistrationView(generics.CreateAPIView):
     """
@@ -37,3 +38,30 @@ class RegistrationView(generics.CreateAPIView):
             'refresh': str(refresh),
             'access': str(refresh.access_token)
         }, status=status.HTTP_201_CREATED)
+
+class LoginView(TokenObtainPairView):
+    """
+    API view to handle user login.
+
+    POST request with the following data in the request body:
+    {
+        "email": string,
+        "password": string
+    }
+
+    Returns a JWT refresh and access token if the user is authenticated successfully.
+    """
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        """
+        This function handles post request for login api
+        """
+        response = super().post(request, *args, **kwargs)
+        refresh_token = response.data['refresh']
+        access_token = response.data['access']
+        response.data = {
+            'refresh': refresh_token,
+            'access': access_token
+        }
+        return response
