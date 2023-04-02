7716843c98feaff2b784b5cbe5f34dc6fd1e63c9
Joylan dsouza <joylan.dsouza@datenwissen.com>
2023-04-02 23:26:21 +0530

    url test cases written for refresh token  urls

--- Backend/main/tests/test_urls.py
+++ Backend/main/tests/test_urls.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse,resolve
-
+from rest_framework_simplejwt.views import TokenRefreshView
 from main.views import (
     RegistrationView,
     LoginView
@@ -15,3 +15,7 @@ class TestUrls(SimpleTestCase):
     def test_login_url_is_resolved(self):
         url = reverse('login')
         self.assertEquals(resolve(url).func.view_class,LoginView)
+
+    def test_refresh_token_url_is_resolved(self):
+        url = reverse('token_refresh')
+        self.assertEquals(resolve(url).func.view_class,TokenRefreshView)
