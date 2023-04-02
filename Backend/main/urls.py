036e6ed97dd50d280933576e86ff5b6b554e8206
Joylan dsouza <joylan.dsouza@datenwissen.com>
2023-04-02 13:19:54 +0530

    logn api added to handle post request

--- Backend/main/urls.py
+++ Backend/main/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import RegistrationView
+from .views import LoginView
 
 urlpatterns = [
     
     # api for user registration
     path('register/', RegistrationView.as_view(), name='register'),
+
+    # api for user login
+    path('login/', LoginView.as_view(), name='login'),
 ]
