77aea4ae9756857bc323b74bbed029ae4b0b5281
Joylan dsouza <joylan.dsouza@datenwissen.com>
2023-04-02 13:01:38 +0530

    api added for user registration

--- Backend/main/urls.py
+++ Backend/main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from .views import RegistrationView
 
 urlpatterns = [
-
+    
+    # api for user registration
+    path('register/', RegistrationView.as_view(), name='register'),
 ]
