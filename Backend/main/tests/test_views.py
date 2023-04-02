b792224004169fbf928862defd4d7012a46b7c12
Joylan dsouza <joylan.dsouza@datenwissen.com>
2023-04-03 01:05:49 +0530

    unit test views written for content api iews

--- Backend/main/tests/test_views.py
+++ Backend/main/tests/test_views.py
@@ -4,7 +4,12 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from main.models import User, Content, Category
 from main.serializers import UserSerializer, ContentSerializer, ContentViewSerializer
 from django.urls import reverse
+from rest_framework.test import APIClient
 import json
+from django.core.files.base import ContentFile
+
+dummy_pdf_content = b'This is a dummy PDF file.'
+dummy_pdf_file = ContentFile(dummy_pdf_content)
 
 client = Client()
 
@@ -84,4 +89,74 @@ class LoginViewTest(TestCase):
             data=json.dumps(self.invalid_payload),
             content_type='application/json'
         )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class ContentListViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(
+            email='testemail@test.com',
+            password='testpassword123',
+            full_name='Test User'
+        )
+        self.category = Category.objects.create(name='Test Category')
+        self.content_data = {
+            "title": "Test Title",
+            "body": "Test Body",
+            "summary": "Test Summary",
+            'pdf_file':dummy_pdf_file,
+            "categories": [self.category.id]
+        }
+        refresh = RefreshToken.for_user(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(refresh.access_token))
+
+    def test_content_list_view_with_authenticated_user(self):
+        response = self.client.get(reverse('content-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_content_list_view_with_unauthenticated_user(self):
+        self.client.credentials()  # remove authentication credentials
+        response = self.client.get(reverse('content-list'))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_content_detail_view_with_authenticated_user(self):
+        content = Content.objects.create(title='Test Title', body='Test Body', summary='Test Summary', pdf_file=dummy_pdf_file, author=self.user)
+        url = reverse('content-detail', kwargs={'pk': content.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_content_detail_view_with_unauthenticated_user(self):
+        content = Content.objects.create(title='Test Title', body='Test Body', summary='Test Summary', pdf_file=dummy_pdf_file, author=self.user)
+        url = reverse('content-detail', kwargs={'pk': content.id})
+        self.client.credentials() # remove authentication credentials
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_content_create_view_with_authenticated_user(self):
+        response = self.client.post(reverse('content-create'), data=self.content_data, format='multipart')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_content_create_view_with_unauthenticated_user(self):
+        self.client.credentials() # remove authentication credentials
+        response = self.client.post(reverse('content-create'), data=self.content_data, format='multipart')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_content_update_view_with_unauthenticated_user(self):
+        content = Content.objects.create(title='Test Title', body='Test Body', summary='Test Summary', pdf_file=dummy_pdf_file, author=self.user)
+        url = reverse('content-update', kwargs={'pk': content.id})
+        self.client.credentials() # remove authentication credentials
+        response = self.client.put(url, data=self.content_data, format='multipart')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_content_delete_view_with_authenticated_user(self):
+        content = Content.objects.create(title='Test Title', body='Test Body', summary='Test Summary', pdf_file=dummy_pdf_file, author=self.user)
+        url = reverse('content-delete', kwargs={'pk': content.id})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_content_delete_view_with_unauthenticated_user(self):
+        content = Content.objects.create(title='Test Title', body='Test Body', summary='Test Summary', pdf_file=dummy_pdf_file, author=self.user)
+        url = reverse('content-delete', kwargs={'pk': content.id})
+        self.client.credentials() # remove authentication credentials
+        response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
